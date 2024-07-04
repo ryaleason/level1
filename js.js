2246271e2d5d0cd2a00ca876ee7c43d1c4cc577f
Ryaleason <113250653+ryaleason@users.noreply.github.com>
2024-07-04 11:14:25 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+// YAHAHAHAHYUK
+console.log("HAI AMAN");
+
+if (true) {
+    console.log("HAI AMAN");
+    document.getElementById("button").classList.add("muncul-password")
+    
+} else {
+    console.log("jnagan munculkan pasword");
+}
